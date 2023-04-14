9f76e3ca8dd55e2997cbc1abc4767600a87f84dd
vn97d <vn97d@umsystem.edu>
2023-04-13 23:55:19 -0500

    commit

--- PDS_Assignment2/Assignment2_C.R
+++ PDS_Assignment2/Assignment2_C.R
@@ -49,4 +49,18 @@ ggplot() +
   geom_vline(xintercept = percentile_bp_population, linetype = "dashed", color = "blue") +
   labs(title = "Comparison of percentile of BloodPressure", x = "BloodPressure", y = "Density", color = "Type") +
   scale_color_manual(values = c("red", "blue"), labels = c("Sample", "Population"))
+
+#Report on Analysis
+
+#The above produces three density plots that compare the population statistics for BloodPressure with the average statistics estimated from bootstrap samples.
+
+#The first plot demonstrates how closely the mean BloodPressure of the population and the average mean BloodPressure of the bootstrap samples are aligned.
+
+#The second plot demonstrates that the average BloodPressure standard deviation for the bootstrap samples is somewhat smaller than the BloodPressure standard deviation for the population.
+
+#The final plot demonstrates how well the average percentile of blood pressure in the bootstrap samples matched the percentile of blood pressure in the general population.
+
+#These plots indicate that, overall, the bootstrap samples give a decent estimate of the population statistics of BloodPressure, especially for the mean and percentile values. To completely comprehend the similarities and differences between the population and bootstrap sample statistics, more investigation would be required.
+
+
        
